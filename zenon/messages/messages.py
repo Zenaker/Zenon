4c4d80eaaa176b841145efcf7aa8654cf86a8b4d
Zen <31873092+Zenaker@users.noreply.github.com>
2018-04-22 17:21:17 -0400

    added proxies

--- zenon/messages/messages.py
+++ zenon/messages/messages.py
@@ -5,28 +5,28 @@ class Messages(object):
         self.token = token
         self.discord = Discord
         
-    def send_message(self, chatid, content): # it can also be use as a private message
-        return requests.post(self.discord + "channels/" + str(chatid) + "/messages#", data={"content":str(content), "nonce":str(chatid)}, headers={"Authorization":self.token}).text
+    def send_message(self, chatid, content, proxy): # it can also be use as a private message
+        return requests.post(self.discord + "channels/" + str(chatid) + "/messages#", proxies=proxy, data={"content":str(content), "nonce":str(chatid)}, headers={"Authorization":self.token}).text
         
-    def typing_action(self, chatid):
-        return requests.post(self.discord + "channels/" + str(chatid) + "/typing", headers={"Authorization":self.token}).text
+    def typing_action(self, chatid, proxy):
+        return requests.post(self.discord + "channels/" + str(chatid) + "/typing", proxies=proxy, headers={"Authorization":self.token}).text
     
     def pinMessage(self, chatid, msgid):
-        return requests.post(self.discord + "channels/" + str(chatid) + "/pins/" + str(msgid), headers={"Authorization":self.token}).text
+        return requests.post(self.discord + "channels/" + str(chatid) + "/pins/" + str(msgid), proxies=proxy, headers={"Authorization":self.token}).text
     
     def deleteMessage(self, chatid, messageid):
-        return requests.delete(self.discord + "channels/" + str(chatid) + "/messages/" + str(messageid), headers={"Authorization":self.token}).text
+        return requests.delete(self.discord + "channels/" + str(chatid) + "/messages/" + str(messageid), proxies=proxy, headers={"Authorization":self.token}).text
     
     def editMessage(self, chatid, messageid, text):
-        return requests.patch(self.discord + "channels/" + str(chatid) + "/messages/" + str(messageid), headers={"Authorization":self.token}, data={"content":text}).text
+        return requests.patch(self.discord + "channels/" + str(chatid) + "/messages/" + str(messageid), proxies=proxy, headers={"Authorization":self.token}, data={"content":text}).text
         
     def sendFile(self, chatid, file, content):
-        return requests.post(self.discord + "channels/" + str(chatid) + "/messages", headers={"Authorization":self.token, "content":str(content)}, files={"file":open(file, 'rb')}).text
+        return requests.post(self.discord + "channels/" + str(chatid) + "/messages", proxies=proxy, headers={"Authorization":self.token, "content":str(content)}, files={"file":open(file, 'rb')}).text
         
     def get_message(self, chatid):
-        res = requests.get(self.discord + "channels/" + str(chatid) + "/messages?limit=1", headers={"Authorization":self.token}).text
+        res = requests.get(self.discord + "channels/" + str(chatid) + "/messages?limit=1", proxies=proxy, headers={"Authorization":self.token}).text
         return res.split('"content": "')[1].split('"')[0]
     
     def get_author(self, chatid):
-        res = requests.get(self.discord + "channels/" + str(chatid) + "/messages?limit=1", headers={"Authorization":self.token}).text
+        res = requests.get(self.discord + "channels/" + str(chatid) + "/messages?limit=1", proxies=proxy, headers={"Authorization":self.token}).text
         return res.split('"username": "')[1].split('"')[0]
