32a517a5e8539cb7f101f4847a5d61d91abad644
Zen <31873092+Zenaker@users.noreply.github.com>
2018-04-22 17:23:48 -0400

    prprprprxoiesss

--- zenon/actions/server.py
+++ zenon/actions/server.py
@@ -5,20 +5,20 @@ class Server(object):
         self.discord = discord
         self.token = token
         
-    def join_server(self, invite):
-        return requests.post(self.discord + "invite/" + invite, headers={"Authorization":self.token})
+    def join_server(self, invite, proxy):
+        return requests.post(self.discord + "invite/" + invite, proxies=proxy, headers={"Authorization":self.token})
         
-    def leave_server(self, serverid):
-        return requests.delete(self.discord + "users/@me/guilds/" + str(serverid), headers={"Authorization":self.token}).text
+    def leave_server(self, serverid, proxy):
+        return requests.delete(self.discord + "users/@me/guilds/" + str(serverid), proxies=proxy, headers={"Authorization":self.token}).text
         
-    def createServer(self, logo, name, region):
-        return requests.post(self.discord + "guilds", headers={"Authorization":self.token}, data={"icon":logo, "name":name, "region":region}).text
+    def createServer(self, logo, name, region, proxy):
+        return requests.post(self.discord + "guilds", headers={"Authorization":self.token}, proxies=proxy, data={"icon":logo, "name":name, "region":region}).text
         
-    def kick(self, chatid, userid, reason):
-        return requests.delete(self.discord + "guilds/" + str(chatid) + "/members/" + str(userid) + "?reason=" + reason, headers={"Authorization":self.token}).text
+    def kick(self, chatid, userid, reason, proxy):
+        return requests.delete(self.discord + "guilds/" + str(chatid) + "/members/" + str(userid) + "?reason=" + reason, proxies=proxy, headers={"Authorization":self.token}).text
         
-    def ban(self, chatid, userid, reason):
-        return requests.put(self.discord + "guilds/" + str(chatid) + "/bans/" + str(userid) + "?delete-message-days=0&reason=" + reason, headers={"Authorization":self.token}).text
+    def ban(self, chatid, userid, reason, proxy):
+        return requests.put(self.discord + "guilds/" + str(chatid) + "/bans/" + str(userid) + "?delete-message-days=0&reason=" + reason, proxies=proxy, headers={"Authorization":self.token}).text
         
     
         
