51106a60f638cd2aa3e2d231f438a3400a77b054
Zen <31873092+Zenaker@users.noreply.github.com>
2018-04-22 17:22:40 -0400

    Update messages.py

--- zenon/messages/messages.py
+++ zenon/messages/messages.py
@@ -11,22 +11,22 @@ class Messages(object):
     def typing_action(self, chatid, proxy):
         return requests.post(self.discord + "channels/" + str(chatid) + "/typing", proxies=proxy, headers={"Authorization":self.token}).text
     
-    def pinMessage(self, chatid, msgid):
+    def pinMessage(self, chatid, msgid, proxy):
         return requests.post(self.discord + "channels/" + str(chatid) + "/pins/" + str(msgid), proxies=proxy, headers={"Authorization":self.token}).text
     
-    def deleteMessage(self, chatid, messageid):
+    def deleteMessage(self, chatid, messageid, proxy):
         return requests.delete(self.discord + "channels/" + str(chatid) + "/messages/" + str(messageid), proxies=proxy, headers={"Authorization":self.token}).text
     
-    def editMessage(self, chatid, messageid, text):
+    def editMessage(self, chatid, messageid, text, proxy):
         return requests.patch(self.discord + "channels/" + str(chatid) + "/messages/" + str(messageid), proxies=proxy, headers={"Authorization":self.token}, data={"content":text}).text
         
-    def sendFile(self, chatid, file, content):
+    def sendFile(self, chatid, file, content, proxy):
         return requests.post(self.discord + "channels/" + str(chatid) + "/messages", proxies=proxy, headers={"Authorization":self.token, "content":str(content)}, files={"file":open(file, 'rb')}).text
         
-    def get_message(self, chatid):
+    def get_message(self, chatid, proxy):
         res = requests.get(self.discord + "channels/" + str(chatid) + "/messages?limit=1", proxies=proxy, headers={"Authorization":self.token}).text
         return res.split('"content": "')[1].split('"')[0]
     
-    def get_author(self, chatid):
+    def get_author(self, chatid, proxy):
         res = requests.get(self.discord + "channels/" + str(chatid) + "/messages?limit=1", proxies=proxy, headers={"Authorization":self.token}).text
         return res.split('"username": "')[1].split('"')[0]
